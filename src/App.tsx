2baf1ff26a1c25c3144d1dab4b6e011fe51b62ae
Rabire <rabireh@outlook.fr>
2024-12-08 13:23:42 +0100

    theme up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Button>Click me</Button>
