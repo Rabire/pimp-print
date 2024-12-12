39c7a3f3be0c488e1ed7eb7ca47d6eba628f0595
Rabire <rabireh@outlook.fr>
2024-12-12 13:42:30 +0100

    canGoToEditor button

--- src/pages/selector/page.tsx
+++ src/pages/selector/page.tsx
@@ -1,9 +1,24 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRight, ShoppingCartIcon } from "lucide-react";
+import { useNavigate } from "react-router";
 import DocumentPreview from "./components/document-preview";
 import PickTemplatePanel from "./components/pick-template-panel";
+import { useSelectorContext } from "./context";
 
 const HomePage = () => {
+  const { sides } = useSelectorContext();
+  const [page1, page2] = sides;
+
+  const canGoToEditor = Boolean(page1 && page2);
+
+  const navigate = useNavigate();
+
+  const onEditorClick = () => {
+    if (!canGoToEditor) return;
+
+    navigate("/editor");
+  };
+
   return (
     <main className="grid grid-cols-3 grid-rows-[1fr_auto]">
       <DocumentPreview />
@@ -17,11 +32,9 @@ const HomePage = () => {
           <span>Vos documents sauvegardés</span>
         </Button>
 
-        <Button asChild>
-          <a href="/editor">
-            <span>Personnaliser le document</span>
-            <ArrowRight />
-          </a>
+        <Button disabled={!canGoToEditor} onClick={onEditorClick}>
+          <span>Personnaliser le document</span>
+          <ArrowRight />
         </Button>
       </div>
     </main>
